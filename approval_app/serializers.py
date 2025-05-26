e9070f3683f8e3292101abeb400af7107f39182f
AnsariMd17 <mohamedansari@dataterrain.com>
2025-05-26 16:14:20 +0530

    updated changes

--- approval_app/serializers.py
+++ approval_app/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Client
+from .models import Client, AdminUser
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = "__all__"
+
+class AdminUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdminUser
+        exclude = [ 'password', 'last_login', 'is_superuser', 'changed_by', 'created_by', 'created_at', 'changed_at' ]
